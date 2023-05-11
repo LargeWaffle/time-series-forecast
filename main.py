70c5e2c55094599319e2452a66a678980bf02225
Thomas Sirvent <sirventhomas@gmail.com>
2023-05-11 18:44:39 -0400

    pca, ft importance, etc

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     train_df, _, _ = read_sales(DATAPATH)
 
     # stationary_test(train_df)
-    xgb_regressor(train_df)
+    xgb_regressor(train_df, show_ft_ip=True, pca=False)
 
     print("\nEnd of program")
--- predictions.py
+++ predictions.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import xgboost as xgb
 from sklearn.decomposition import PCA
 from sklearn.metrics import mean_squared_error, mean_absolute_error
@@ -9,40 +9,71 @@ from sklearn.linear_model import LinearRegression
 from statsmodels.tsa.stattools import adfuller
 
 
+def plot_predictions(x_val, y_val, y_pred):
+
+    samples = list(range(0, len(x_val)))
+    plt.figure(figsize=(10, 6))
+    plt.plot(samples, y_val, label='Expected', alpha=0.5)
+    plt.plot(samples, y_pred, label='Predicted', alpha=0.5)
+    plt.legend(loc="upper right")
+    plt.show()
+
+    plt.plot(samples, abs(y_val - y_pred), label='Difference')
+    plt.legend(loc="upper right")
+    plt.show()
+
+
 def lreg():
     model = LinearRegression()
     # model.fit(x_train, y)
 
 
-def xgb_regressor(train_df):
+def xgb_regressor(train_df, show_ft_ip=False, pca=False):
     x = train_df.drop(['id', 'store_nbr', 'sales', 'dcoilwtico'], axis=1)
     y = train_df['sales']
 
-    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=42)
+    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.15, random_state=42)
 
     scaler = MinMaxScaler()
     x_train = scaler.fit_transform(x_train)
     x_val = scaler.transform(x_val)
 
-    pca = PCA(n_components=0.95)
-    x_train = pca.fit_transform(x_train)
-    x_val = pca.transform(x_val)
+    feature_names = list(x.columns)
+
+    if pca:
+        pca = PCA(n_components=0.95)
+        x_train = pca.fit_transform(x_train)
+        x_val = pca.transform(x_val)
+
+        n_pcs = pca.components_.shape[0]
+
+        # get the most important feature on EACH component
+        most_important = [np.abs(pca.components_[i]).argmax() for i in range(n_pcs)]
+
+        # get the names
+        most_names = [feature_names[most_important[i]] for i in range(n_pcs)]
+
+        feature_names = ['PC{}_{}'.format(i + 1, most_names[i]) for i in range(n_pcs)]
 
     model = xgb.XGBRegressor()
     model.fit(x_train, y_train)
-    model.get_booster().feature_names = list(x.columns)
 
-    xgb.plot_importance(model, importance_type='weight', xlabel='weight', ylabel='features',
-                        height=0.6, show_values=False, title='XGBoost feature importance')
-    plt.show()
+    if show_ft_ip:
+        model.get_booster().feature_names = feature_names
+
+        _, ax = plt.subplots(figsize=(10, 6))
+        xgb.plot_importance(model, importance_type='weight', xlabel='weight', ylabel='features',
+                            height=0.6, show_values=False, grid=False, ax=ax,
+                            title='XGBoost feature importance')
+        plt.show()
 
     y_pred = model.predict(x_val)
     y_pred[y_pred < 0] = 0
 
-    mae = mean_absolute_error(y_val, y_pred)
-    mse = mean_squared_error(y_val, y_pred)
-    print(f'MAE: {mae:.2f}')
-    print(f'MSE: {mse:.2f}')
+    print('MAE: {:.2f}'.format(mean_absolute_error(y_val, y_pred)))
+    print('MSE: {:.2f}'.format(mean_squared_error(y_val, y_pred)))
+
+    plot_predictions(x_val, y_val, y_pred)
 
 
 def stationary_test(df):
