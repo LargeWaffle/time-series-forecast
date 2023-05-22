31ac149ac7645667d9065049c9a8ebada3821569
Thomas Sirvent <sirventhomas@gmail.com>
2023-05-22 11:10:12 -0400

    full refactor with specialized models

---
+++ main.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import xgboost as xgb
+
+from salesft import read_sales
+
+DATAPATH = "data/store-sales"
+
+
+def show_feature_importance(self, ft_val):
+    plt.figure(figsize=(12, 6))
+
+    (pd.Series(ft_val, index=self.feature_names)
+     .sort_values(ascending=True)
+     .plot(kind='barh'))
+
+    plt.show()
+
+
+def plot_predictions(nb_samples, y_val, y_pred):
+    sp_list = list(range(0, nb_samples))
+    plt.figure(figsize=(10, 6))
+
+    plt.plot(sp_list, y_val, label='Expected', alpha=0.5)
+    plt.plot(sp_list, y_pred, label='Predicted', alpha=0.5)
+    plt.legend(loc="upper right")
+    plt.show()
+
+    plt.plot(sp_list, abs(y_val - y_pred), label='Difference')
+    plt.legend(loc="upper right")
+    plt.show()
+
+
+def create_submission(models_dict, test_df, drop_cols):
+    submission_df = pd.read_csv(DATAPATH + '/sample_submission.csv')
+
+    for fam, model in models_dict.items():
+        current_df = test_df[fam]
+        fam_df = current_df.drop(drop_cols, axis=1, errors='ignore')
+
+        sales_values = model.predict(fam_df)
+        sales_values[sales_values < 0] = 0
+        
+        current_df['sales'] = sales_values
+        current_df = current_df[['id', 'sales']]
+
+        submission_df = submission_df.merge(current_df, on='id', how='left')
+        submission_df['sales'] = submission_df['sales_y'].fillna(submission_df['sales_x'])
+        submission_df = submission_df.drop(['sales_x', 'sales_y'], axis=1)
+
+    submission_df.to_csv('submission.csv', index=False)
+
+    print("Submission saved!")
+
+
+if __name__ == '__main__':
+    split_size = 0.8
+    drop_cols = ['id', 'sales']
+
+    train_data, test_data = read_sales(DATAPATH, StandardScaler(), False)
+
+    family_models = {}
+    for family, data_df in train_data.items():
+        family_model = xgb.XGBRegressor(n_estimators=200, importance_type='gain', eval_metric='rmse',
+                                        early_stopping_rounds=20, verbosity=1)
+
+        train_size = int(len(data_df) * split_size)
+        val_size = len(data_df) - train_size
+        train_df, val_df = data_df[:train_size], data_df[train_size:]
+
+        x_train, y_train = train_df.drop(drop_cols, axis=1), train_df['sales']
+        x_val, y_val = val_df.drop(drop_cols, axis=1), val_df['sales']
+
+        family_model.fit(x_train, y_train, eval_set=[(x_val, y_val)])
+
+        print(f"\nEvaluating family {family}")
+        y_pred = family_model.predict(x_val)
+        y_pred[y_pred < 0] = 0
+
+        print("\nRegression metrics")
+        print('MAE: {:.2f}'.format(mean_absolute_error(y_val, y_pred)))
+        print('MSE: {:.2f}'.format(mean_squared_error(y_val, y_pred, squared=True)))
+        print('RMSE: {:.2f}'.format(mean_squared_error(y_val, y_pred, squared=False)))
+        print('R2: {:.2f}'.format(r2_score(y_val, y_pred)))
+
+        family_models[family] = family_model
+
+    create_submission(family_models, test_data, drop_cols)
+
+    print("\nEnd of program")
--- salesft.py
+++ salesft.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import LabelEncoder
+
 from statsmodels.tsa.deterministic import DeterministicProcess, CalendarFourier
 
 
@@ -17,11 +18,9 @@ def assign_time_ft(df):
     df['is_weekday'] = 0
     df.loc[df['weekday'] < 5, 'is_weekday'] = 1
 
-    df["season"] = np.where(df.month.isin([12, 1, 2]), 0, 1)
-    df["season"] = np.where(df.month.isin([6, 7, 8]), 2, df["season"])
-    df["season"] = pd.Series(np.where(df.month.isin([9, 10, 11]), 3, df["season"])).astype("int8")
+    df["season"] = df["month"] % 12 // 3
 
-    df['earthquake_rl'] = np.where(df['description'].str.contains('Terremoto Manabi'), 0, 1)
+    df['earthquake_rl'] = np.where(df['description'].str.contains('Terremoto Manabi'), 1, 0)
 
     return df
 
@@ -30,31 +29,17 @@ def handle_na(df):
     df['transferred'] = df['transferred'].fillna(False).astype(int)
     df['is_holiday'] = df['is_holiday'].fillna(0).astype(int)
 
+    df['description'] = df['description'].fillna('None')
+
     df['holiday_type'] = df['holiday_type'].fillna('Common')
     df['locale'] = df['locale'].fillna('Common')
     df['locale_name'] = df['locale_name'].fillna('Ecuador')  # WARNING
 
-    df['transactions'] = df['transactions'].fillna(0).astype(int)
     df['dcoilwtico'] = df['dcoilwtico'].fillna(method='backfill')
 
+    df['transactions'] = df['transactions'].fillna(0).astype(int)
+
     return df
-
-
-"""def fourier(df):
-    # TODO: Test
-    # DeterministicProcess
-    fourier_a = CalendarFourier(freq='A', order=5)
-    fourier_m = CalendarFourier(freq='M', order=2)
-    fourier_w = CalendarFourier(freq='W', order=4)
-
-    dp = DeterministicProcess(index=df.index,
-                              order=1,
-                              seasonal=False,
-                              constant=False,
-                              additional_terms=[fourier_a, fourier_m, fourier_w],
-                              drop=True)
-    dp_df = dp.in_sample()
-    return pd.concat([df, dp_df], axis=1)"""
 
 
 def encode_ft(df):
@@ -74,26 +59,32 @@ def lag_ft(df, lag_infos):
     for col_name, lags in lag_infos.items():
         for lag in lags:
             df[f'{col_name}_{lag}'] = df[col_name].shift(lag)
+            df[f'{col_name}_{lag}'] = df[f'{col_name}_{lag}'].fillna(0).astype(float)
 
     return df
 
 
 def window_ft(df):
-    df['oil_week_avg'] = df['dcoilwtico'].rolling(7).mean()
-    df['oil_week_min'] = df['dcoilwtico'].rolling(7).min()
-    df['oil_week_max'] = df['dcoilwtico'].rolling(7).max()
+    times = {'week': 7, '2weeks': 14, 'month': 28}
 
-    df['oil_2weeks_avg'] = df['dcoilwtico'].rolling(14).mean()
-    df['oil_2weeks_min'] = df['dcoilwtico'].rolling(14).min()
-    df['oil_2weeks_max'] = df['dcoilwtico'].rolling(14).max()
+    for key, val in times.items():
+        df[f'oil_{key}_avg'] = df['dcoilwtico'].rolling(val).mean()
+        df[f'oil_{key}_avg'] = df[f'oil_{key}_avg'].fillna(0).astype(float)
 
-    df['oil_month_avg'] = df['dcoilwtico'].rolling(28).mean()
-    df['oil_month_min'] = df['dcoilwtico'].rolling(28).min()
-    df['oil_month_max'] = df['dcoilwtico'].rolling(28).max()
+        df[f'oil_{key}_min'] = df['dcoilwtico'].rolling(val).min()
+        df[f'oil_{key}_min'] = df[f'oil_{key}_min'].fillna(0).astype(float)
+
+        df[f'oil_{key}_max'] = df['dcoilwtico'].rolling(val).max()
+        df[f'oil_{key}_max'] = df[f'oil_{key}_max'].fillna(0).astype(float)
 
     df['avg_transactions'] = df['transactions'].rolling(15, min_periods=10).mean()
+    df['avg_transactions'] = df['avg_transactions'].fillna(0).astype(float)
+
     df['min_transactions'] = df['transactions'].rolling(15, min_periods=10).min()
+    df['min_transactions'] = df['min_transactions'].fillna(0).astype(float)
+
     df['max_transactions'] = df['transactions'].rolling(15, min_periods=10).max()
+    df['max_transactions'] = df['max_transactions'].fillna(0).astype(float)
 
     return df
 
@@ -102,52 +93,60 @@ def format_sales(df, data_path):
     stores_df = pd.read_csv(data_path + '/stores.csv')
     stores_df = stores_df.rename(columns={'type': 'store_type'})
 
-    oil_df = pd.read_csv(data_path + '/oil.csv', parse_dates=['date'])
     transactions_df = pd.read_csv(data_path + '/transactions.csv', parse_dates=['date'])
-
-    df = df.merge(stores_df, on='store_nbr', how='left')
-    df = df.merge(oil_df, on='date', how='left')
-    df = df.merge(transactions_df, on=['date', 'store_nbr'], how='left')
+    oil_df = pd.read_csv(data_path + '/oil.csv', parse_dates=['date'])
 
     holidays_df = pd.read_csv(data_path + '/holidays_events.csv', parse_dates=['date'])
     holidays_df = holidays_df.rename(columns={'type': 'holiday_type'})
     holidays_df['is_holiday'] = 1
 
+    df = df.merge(stores_df, on='store_nbr', how='left')
+    df = df.merge(oil_df, on='date', how='left')
+    df = df.merge(transactions_df, on=['date', 'store_nbr'], how='left')
     df = df.merge(holidays_df, on='date', how='left')
 
     df = handle_na(df)
     df.loc[df['transferred'] == 1, 'is_holiday'] = 0
 
     df = encode_ft(df)
+    df = assign_time_ft(df)
+    df = df.drop(['description'], axis=1)
 
     lag_features = {
         'dcoilwtico': [1, 3, 7, 14],
         'transactions': [1, 3, 7, 14]
     }
 
-    df = assign_time_ft(df)
-    df = df.drop(['description'], axis=1)
+    for family in df['family'].unique():
 
-    df = lag_ft(df, lag_features)
-    df = window_ft(df)
+        sub_df = df.loc[df['family'] == family]
 
-    df[df.select_dtypes(np.int64).columns] = df.select_dtypes(np.int64).astype(np.int32)
+        sub_df = lag_ft(sub_df, lag_features)
+        sub_df = window_ft(sub_df)
+        df.loc[sub_df.index, sub_df.columns] = sub_df
+
+    df[df.select_dtypes(np.int32).columns] = df.select_dtypes(np.int32).astype(np.int64)
     df[df.select_dtypes(np.float32).columns] = df.select_dtypes(np.float32).astype(np.float64)
 
-    return df
+    return df.set_index('date')
 
 
-def read_sales(data_path):
+def read_sales(data_path, scaler, use_pca=False):
     train_df = pd.read_csv(data_path + '/train.csv', parse_dates=['date'])
     test_df = pd.read_csv(data_path + '/test.csv', parse_dates=['date'])
 
-    data_df = pd.concat([train_df, test_df], axis=0)
-    data_df = format_sales(data_df, data_path)
-    data_df = data_df.set_index('date')
+    train_df = format_sales(train_df, data_path)
+    scaled = train_df.drop(['id', 'sales'], axis=1)
+    train_df[scaled.columns] = scaler.fit_transform(scaled[scaled.columns])
 
-    train_df = data_df[data_df.index <= pd.to_datetime("2017-08-15")]
-    train_df = train_df.dropna()
+    test_df = format_sales(test_df, data_path)
+    scaled = test_df.drop(['id'], axis=1)
+    test_df[scaled.columns] = scaler.transform(scaled[scaled.columns])
 
-    test_df = data_df[data_df.index > pd.to_datetime("2017-08-15")]
+    train_data = {}
+    test_data = {}
+    for idx, famval in enumerate(train_df['family'].unique()):
+        train_data[idx] = train_df.loc[train_df['family'] == famval]
+        test_data[idx] = test_df.loc[test_df['family'] == famval]
 
-    return train_df, test_df
+    return train_data, test_data
